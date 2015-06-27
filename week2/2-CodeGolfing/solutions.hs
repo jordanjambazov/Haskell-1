9f0ad276ede1a9230c4ab3fde92d14f1b5c17671
Jordan Jambazov <jordan.jambazov@gmail.com>
2015-06-27 13:16:55 +0300

    Add mapMap, filterFilter, unit, row, transpose

--- week2/2-CodeGolfing/solutions.hs
+++ week2/2-CodeGolfing/solutions.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 -- 01. Map as foldl
 -- Implement the function `map` using `foldl`
 map' :: (a -> b) -> [a] -> [b]
@@ -34,3 +36,31 @@ localMaxima xs = h xs []
   where h (x:y:z:xs) a | x < y && y > z = h (y:z:xs) $ a ++ [y]
                        | otherwise      = h (y:z:xs) a
         h _          a                  = a
+
+-- 08. Map a function to a list of lists
+mapMap :: (a -> b) -> [[a]] -> [[b]]
+mapMap f []  = []
+mapMap f (x:xs) = (map f x) : (mapMap f xs)
+
+-- 09. Filter a list of lists
+filterFilter :: (a -> Bool) -> [[a]] -> [[a]]
+filterFilter f []  = []
+filterFilter f (x:xs) = (filter f x) : (filterFilter f xs)
+
+-- 10. Generate the unit matrix by given element and dimensions
+unit :: (Num a1, Num a, Ord a1) => a -> a1 -> [[a]]
+unit item size = matrix 0 []
+  where row index num acc    | num == index  = item : (row (index + 1) num acc)
+                             | index < size  = 0 : row (index + 1) num acc
+                             | otherwise     = acc
+        matrix index acc     | index < size = (row 0 index []):(matrix (index + 1) acc)
+                             | otherwise = acc
+
+-- 11. Get the nth row and column of a matrix
+row :: Int -> [[a]] -> [a]
+row nth xs = xs !! nth
+
+-- 12. Transpose a matrix
+transpose' :: [[a]] -> [[a]]
+transpose' ([]:xs) = []
+transpose' m = map (!! 0) m : transpose' (map tail m)
